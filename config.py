d332971aa4c7be87942bebf36dff00ef7ae6b7de
izeint <136033636+izeint@users.noreply.github.com>
2026-02-19 01:52:14 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "bibliotheque.db")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "bibliotheque.db")
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
